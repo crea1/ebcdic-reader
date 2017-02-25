37b730f556ce8cba68de00e7527cd39ba62bb202
Marius Kristensen <marius.kristensen@gmail.com>
2017-02-25 21:56:44 +0100

    Use map to map from ebcdic to ascii and print result

--- ebcdic_reader.go
+++ ebcdic_reader.go
@@ -9,8 +9,6 @@ import (
 )
 
 func main() {
-
-	fmt.Println("Hello :)")
 	file, err := os.Open("TEST.EBC")
 	if err != nil {
 		log.Fatal(err)
@@ -18,8 +16,10 @@ func main() {
 
 	defer file.Close()
 
-	reader := bufio.NewReader(file)
+	var result []byte
+	byteCounter := 0
 
+	reader := bufio.NewReader(file)
 	for {
 		b, err := reader.ReadByte()
 		if err != nil {
@@ -28,9 +28,20 @@ func main() {
 			}
 			log.Fatal(err)
 		} else {
+			byteCounter++
+			// For some reason I only get CR and not CRLF from EBCDIF file. Add LF for printing
+			if b == 0x0d {
+				result = append(result, 0x0d, 0x0a)
+			}
+			if val, ok := ebcdic_to_ascii[b]; ok {
+				result = append(result, val)
+			}
 
-			fmt.Println(b)
+
 		}
 
 	}
+
+	//fmt.Printf("Read bytes : %d Output bytes : %d\n", byteCounter, len(result))
+	fmt.Println(string(result[:]))
 }
